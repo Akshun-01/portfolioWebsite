6f218b274e3c947d9af4b0e2cf0e95b73d1885a6
Akshun-01 <akshunkuthiala2002@gmail.com>
2023-09-19 01:33:32 +0530

    make mobile responsive and add walking animation to ninja

--- src/components/ui/FooterNinja.jsx
+++ src/components/ui/FooterNinja.jsx
@@ -3,8 +3,9 @@ const FooterNinja = () => {
   document.getElementById('ninja');
 
   return (
-    <div className="w-screen z-10  cursor-pointer fixed bottom-0 left-0">
-        <img id="ninja" src="./assets/ninja.png" alt="" className="h-20 aspect-square" />
+    <div className="w-screen z-10  cursor-pointer fixed left-0 bottom-0 animate-walking-ninja">
+        {/* <img id="ninja" src="./assets/ninja.png" alt="" className="h-20 aspect-square" /> */}
+        <img src="./assets/walkingNinja.gif" alt="" className="h-20 aspect-square" />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,15 @@ export default {
         'header-primary': "#C3AA13",
         'background-color': "#141414"
       },
+      keyframes: {
+        walk: {
+          '0%': { transform: 'translate(-2vw)' },
+          '100%': { transform: 'translate(102vw)' },
+        },
+      },
+      animation: {
+        'walking-ninja': 'walk 50s linear infinite',
+      },
     },
   },
   plugins: [],

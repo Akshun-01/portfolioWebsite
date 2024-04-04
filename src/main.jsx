f55863c048f877001218442ad521b1353dc23db5
Akshun-01 <akshunkuthiala2002@gmail.com>
2024-04-05 00:20:53 +0530

    change router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {HashRouter} from 'react-router-dom'
+import {ReactRouter} from 'react-router-dom'
 import './index.scss';
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <ReactRouter>
       <App />
-    </HashRouter>
+    </ReactRouter>
   </React.StrictMode>
 );
